9703308021c100f42e88fce29d5d42aa42e6182e
Omkar Shelar <omkarshelar24@gmail.com>
2018-02-09 20:59:51 +0530

    Added test route to test auto deploy

--- main.py
+++ main.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def index():
 	return '<h1>Hello World!</h1>'
 
+@app.route('/test', methods=['GET'])
+def test_route():
+	return '<h1>Auto Deploy Working!</h1>'
+
 if __name__=='__main__':
-	app.run(debug=True)
+	app.run()
